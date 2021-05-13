836e4735080e93654c5e943b32d03b24c3ec4bea
Bohdan Shevchuk <shevdan007@gmail.com>
2021-05-14 02:14:55 +0300

    Fixed bug with archiving

--- image_augment.py
+++ image_augment.py
@@ -1,6 +1,6 @@
 
 import zipfile, shutil, sys
-from os import path
+import os
 from pathlib import Path
 
 import numpy as np
@@ -15,7 +15,7 @@ from sklearn.model_selection import train_test_split
 
 class ImageAugment:
     def __init__(self, dir_path: str):
-        if not path.exists(dir_path):
+        if not os.path.exists(dir_path):
             raise TypeError("Argument must be a valid path")
         self.fullpath = dir_path
         zipname = dir_path.split('/')
@@ -61,25 +61,58 @@ class ImageAugment:
     def process_zip(self,number_mult, dir_path):
         '''Iterates over the folders in a zip file and  augments each png file'''
         for filename in dir_path.iterdir():
-            if path.isdir(str(filename)):
+            if os.path.isdir(str(filename)):
                 print(f'Processing {str(filename).split("/")[-1]}')
                 self.process_zip(number_mult, Path(str(filename)))
             else:
-                self.augment_image(str(filename), number_mult)
+                try:
+                    self.augment_image(str(filename), number_mult)
+                except AttributeError:
+                    continue
 
     def zip_files(self):
         '''
         zips the file and removes the remporary directory
         '''
-        with zipfile.ZipFile(self.fullpath, 'w') as file:
-            for filename in self.temp_directory.iterdir():
-                file.write(str(filename), filename.name)
+        # zipf = zipfile.ZipFile(self.fullpath, 'w', zipfile.ZIP_DEFLATED)
+
+        # for root, dirs, files in os.walk(str(self.temp_directory)):
+        #     # root = root.split('/')
+        #     # root[0] = root[0].split('-')
+        #     # root[0] = root[0][-1]
+        #     # root = '/'.join(root)
+        #     print(root, dirs, files)
+        #     print(type(root))
+        #     for file in files:
+        #         zipf.write(os.path.join(root, file), 
+        #                 os.path.relpath(os.path.join(root, file), 
+        #                                 os.path.join(str(self.temp_directory), '..')))
+        # zipf.close()
+
+        dir_path = self.fullpath[:-4]
+        root_dir = '/'.join(str(self.temp_directory).split('/')[:-1])
+        print(dir_path,'\n',  root_dir)
+        shutil.make_archive(dir_path, 'zip', str(self.temp_directory))
+
+        # d = str(self.temp_directory)
+
+        # os.chdir(os.path.dirname(d))
+        # with zipfile.ZipFile(dir_path + '.zip',
+        #                     "w",
+        #                     zipfile.ZIP_DEFLATED,
+        #                     allowZip64=True) as zf:
+        #     for root, _, filenames in os.walk(os.path.basename(d)):
+        #         for name in filenames:
+        #             name = os.path.join(root, name)
+        #             name = os.path.normpath(name)
+        #             zf.write(name, name)
         shutil.rmtree(str(self.temp_directory))
 
     
 if __name__ == '__main__':
     import scipy
-    augm = ImageAugment('/Users/shevdan/Documents/Programming/Python/semester2/GroupProject/Cyrillic.zip')
+    from pathlib import Path
+    augm = ImageAugment('/Users/shevdan/Documents/Programming/Python/semester2/groupProject2/random.zip')
     print(augm.fullpath)
     print(augm.zipname)
     print(scipy.__version__)
@@ -87,3 +120,5 @@ if __name__ == '__main__':
     augm.process_zip(2, augm.temp_directory)
     augm.zip_files()
     # augm.augment_image('/Users/shevdan/Documents/Programming/Python/semester2/GroupProject/random/5a2f3c19c27bb.png', 10)
+    # augm.process_zip(2, Path('/Users/shevdan/Documents/Programming/Python/semester2/GroupProject/random'))
+    # augm.augment_image(None, 2)
