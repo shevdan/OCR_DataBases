6de7722a4334ad06de1dadde0c326ce6f4aa7323
oleksadobush <oleksandra.stasiuk@ucu.edu.ua>
2021-05-15 15:08:40 +0300

    fix files

--- expand_csv.py
+++ expand_csv.py
@@ -1,5 +1,3 @@
-
-
 import zipfile, shutil, sys
 import os
 from pathlib import Path
@@ -19,8 +17,9 @@ from tensorflow.keras import utils
 from sklearn.model_selection import train_test_split
 from data_adt import AbstractAugment
 
+
 class CSVAugment(AbstractAugment):
-    def __init__(self, dir_path,im_size=(28,28), output=None) -> None:
+    def __init__(self, dir_path, im_size=(28, 28), output=None) -> None:
         super().__init__(dir_path)
         direct = '/'.join(dir_path.split('/')[:-1])
         if output is None:
@@ -30,13 +29,11 @@ class CSVAugment(AbstractAugment):
         self.im_size = im_size
         if not os.path.exists(str(self.output)):
             self.output.mkdir()
-        
-
 
     def unzip_files(self):
-        '''
+        """
         exctracts the zip file into the temporary directory
-        '''
+        """
         try:
             self.output.mkdir()
         except FileExistsError:
@@ -47,8 +44,8 @@ class CSVAugment(AbstractAugment):
             zip.extractall(str(self.output))
 
     def process_folder(self, dir_path):
-        '''recursively walks through all the directories located by the dir_path
-        and applies augment_image to every image'''
+        """recursively walks through all the directories located by the dir_path
+        and applies augment_image to every image"""
         if isinstance(dir_path, str):
             dir_path = Path(dir_path)
         for filename in dir_path.iterdir():
@@ -61,24 +58,22 @@ class CSVAugment(AbstractAugment):
                     pass
                     # self.augment_image(str(filename), number_mult)
 
-
     def convert_csv_to_img(self, filename):
-        '''
+        """
         Method converts an image into numpy array and adds it to csv file
         with a letter or ciffre on the front of the array as indicator of
         the symbol. Disclaimer: for expanding images for further ML learning
         Image must be a single image of hte symbol and to be contained in a
         folder which is called by the name of the symbol
-        '''
+        """
         data = np.loadtxt(filename, skiprows=1, delimiter=',')
         i = 0
         for row in data:
-            i+=1
+            i += 1
             symb, pixels = row[0], row[1:]
             self.pixels_to_img(pixels, symb, i)
             if i == 10:
                 break
-
 
     def pixels_to_img(self, pixels, symb, cnt):
         if 0 <= symb < 10:
@@ -87,29 +82,21 @@ class CSVAugment(AbstractAugment):
             symb = chr(symb)
         print(pixels.size)
         # pixels = pixels.reshape(28, 28)
-        pixels = pixels.reshape(int(sqrt(pixels.size)),int(sqrt(pixels.size)))
+        pixels = pixels.reshape(int(sqrt(pixels.size)), int(sqrt(pixels.size)))
         image = Image.fromarray(pixels)
         image.resize(self.im_size)
         image = image.convert("L")
         image.show()
-        symb_directory = str(self.output) + '/' +  symb
+        symb_directory = str(self.output) + '/' + symb
 
         img_name = f'{symb_directory}/{symb}_{cnt}.png'
-        if not  os.path.isdir(symb_directory):
+        if not os.path.isdir(symb_directory):
             Path(symb_directory).mkdir()
         image.save(img_name)
 
-
-
     def zip_files(self):
-            # dir_path = self.fullpath[:-4]
-            # root_dir = '/'.join(str(self.temp_directory).split('/')[:-1])
-            # print(dir_path,'\n',  root_dir)
-            shutil.make_archive(str(self.output), 'zip', str(self.output))
-            shutil.rmtree(str(self.temp_directory))
-
-
-if __name__ == '__main__':
-    expand = CSVAugment('/Users/shevdan/Documents/Programming/Python/semester2/groupProject2/random')
-    # expand.convert_img_to_csv('/Users/shevdan/Documents/Programming/Python/semester2/groupProject2/2021-05-14 20.35.49.jpg', '/Users/shevdan/Documents/Programming/Python/semester2/groupProject2/train.csv')
-    expand.process_folder('/Users/shevdan/Documents/Programming/Python/semester2/groupProject2/test')
+        # dir_path = self.fullpath[:-4]
+        # root_dir = '/'.join(str(self.temp_directory).split('/')[:-1])
+        # print(dir_path,'\n',  root_dir)
+        shutil.make_archive(str(self.output), 'zip', str(self.output))
+        shutil.rmtree(str(self.temp_directory))
