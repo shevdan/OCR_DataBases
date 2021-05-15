56549c73d9c256b5f97abda4ceb2ea8962d6081c
oleksadobush <oleksandra.stasiuk@ucu.edu.ua>
2021-05-15 15:10:27 +0300

    fix files

--- ocr_azure.py
+++ ocr_azure.py
@@ -1,7 +1,6 @@
-
-'''
+"""
 Module implementing test ADT to use Microsoft Azure API
-'''
+"""
 
 import os
 import json
@@ -9,8 +8,8 @@ from typing import Type
 import requests
 
 
-class OCR():
-    '''
+class OCR:
+    """
     ADT that enables text recognition of any amount of images stored in one directory.
     Takes path to the directory with stored images, name of the file to store the
     recognized text and optionally language of the recognition when created the instance of the ADT
@@ -32,7 +31,7 @@ class OCR():
         string that defines the name of the file that will contain the recognized text
     language
         optional parametr defining the text language to be recognized.
-        Default value is 'en' for English. Possible values include: 'en', 
+        Default value is 'en' for English. Possible values include: 'en',
         'es', 'fr', 'de', 'it', 'nl', 'pt'. Azure OCR v. 3.2 is awaited to
         be implemented in the code to support over 70 languages in near future.
     headers
@@ -52,7 +51,7 @@ class OCR():
     handler()
         iterates over each file within a directory, sends requests to the API
         to recognize text, gets the recognized text and saves it into the file.
-    '''
+    """
     API_KEY = '0ec5c956c2f74b279cf52c0706dbe7cf'
     ENDPOINT = 'https://westeurope.api.cognitive.microsoft.com/vision/v1.0/ocr'
 
@@ -62,19 +61,19 @@ class OCR():
         self.img_directory = img_directory
         self.filename = output_file_name
         self.language = language
-        self.headers  = {
+        self.headers = {
             'Ocp-Apim-Subscription-Key': self.API_KEY,
             'Content-Type': 'application/octet-stream'
         }
-        self.params   = {
+        self.params = {
             'language': self.language,
             'detectOrientation ': 'true'
         }
 
     def get_text(self, pathToImage):
-        '''
+        """
         Accesses API to get json file containing recognized text
-        '''
+        """
         payload = open(pathToImage, 'rb').read()
         response = requests.post(self.ENDPOINT, headers=self.headers, params=self.params, data=payload)
         results = json.loads(response.content)
@@ -82,32 +81,27 @@ class OCR():
         return results
 
     def parse_text(self, results):
-        '''
+        """
         Parses the information from json file
-        '''
+        """
         text = ''
         for region in results['regions']:
             for line in region['lines']:
                 for word in line['words']:
                     text += word['text'] + ' '
                 text += '\n'
-        return text  
+        return text
 
     def handler(self):
-        '''
+        """
         handles all the images from the directory with images and saves
         recognized text in output.txt
-        '''
+        """
         text = ''
         for filename in sorted(os.listdir(self.img_directory)):
-            if filename.endswith(".jpeg") or filename.endswith(".jpg") or filename.endswith(".png"): 
+            if filename.endswith(".jpeg") or filename.endswith(".jpg") or filename.endswith(".png"):
                 pathToImage = f'{self.img_directory}/{filename}'
                 results = self.get_text(pathToImage)
                 text += self.parse_text(results)
 
         open(self.filename, 'w').write(text)
-
-
-if __name__ == '__main__':
-    ocr = OCR('/Users/shevdan/Documents/Programming/Python/semester2/GroupProject/TextRecognition/examples/images','output.txt')
-
